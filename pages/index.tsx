a97f651b819e53c4f8959e3eb39632a5ad6643a7
Emily Charles <awordforthat@gmail.com>
2021-10-03 21:31:14 -0400

    apply linting and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,22 +13,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          This is my website!
-        </h1>
+        <h1 className={styles.title}>This is my website!</h1>
 
-        <p className={styles.description}>
-         TODO: add some stuff
-        </p>
-
-      
+        <p className={styles.description}>TODO: add some stuff</p>
       </main>
 
       <footer className={styles.footer}>
         <p>Footer goes here</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
