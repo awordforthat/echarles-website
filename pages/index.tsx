7c4afdba2f5c0744a0bb0e3b3582e89cba3793f9
Emily Charles <awordforthat@gmail.com>
2021-10-04 21:27:04 -0400

    add temp styles, content

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,52 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Title</title>
+        <title>Oh hi!</title>
         <meta name="description" content="Emily Charles - personal website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>This is my website!</h1>
+        <section id="greeting">
+          <h1 className={styles.title}>Hi, I'm Emily!</h1>
+          <h2 className={styles.subtitle}>
+            Software engineer, serial dabbler, language enthusiast
+          </h2>
+        </section>
 
-        <p className={styles.description}>TODO: add some stuff</p>
+        <section id="about-me" className={styles.about}>
+          <div className={styles.center}>
+            I'm a software engineer, and I like to work on the borders of
+            things.
+          </div>
+          <div className={styles.center}>
+            That's where the most interesting things happen.
+          </div>
+          <div className={styles.center}>
+            I like controlling the real world from the virtual world (Level99)
+          </div>
+          <div className={styles.center}>
+            Or the virtual world from the real world (ChoreoV)
+          </div>
+          <div className={styles.center}>
+            I like making hybrid things (bionic leg)
+          </div>
+          <div className={styles.center}>
+            and nerdy linguistic things (rhyme detector)
+          </div>
+          <div className={styles.center}>
+            and things that connect people with different interests (KiC)
+          </div>
+          <div className={styles.center}>More detail to come!</div>
+        </section>
       </main>
 
       <footer className={styles.footer}>
-        <p>Footer goes here</p>
+        <ul>
+          <li>Email me</li>
+          <li>Resume (PDF)</li>
+          <li>Woodworking</li>
+        </ul>
       </footer>
     </div>
   );
