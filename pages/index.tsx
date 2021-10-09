15e579d361fce396dff64a0084754869280ad464
Emily Charles <emily.w.charles@gmail.com>
2021-10-08 23:44:48 -0400

    fussing with content and spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,14 +21,26 @@ const Home: NextPage = () => {
         </section>
 
         <section id="about-me" className={styles.about}>
+          <h2>About Me</h2>
           <div className={styles.center}>
-            I'm a software engineer, and I like to work between domains. The
-            most interesting problems
+            I'm a software engineer, and I like working at the borders of
+            things. I've worked on projects that merge speech and software,
+            physiotherapy and costume design, the physical world and the virtual
+            world, and more.
+          </div>
+          <div>
+            I enjoy sharing knowledge, and I'm dedicated to creating and
+            participating in spaces that foster open communication around
+            technical subjects.
           </div>
         </section>
       </main>
 
       <footer className={styles.footer}>
+        <div id="construction" className={styles.center}>
+          This site is perpetually under construction. (I'm currently using it
+          to learn Next.JS, very exciting.) Please forgive any rough edges!
+        </div>
         <ul>
           <li>Email me</li>
           <li>Resume (PDF)</li>
